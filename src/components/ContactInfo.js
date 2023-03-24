4c2c2e5300e0249827b59d47aebfcd9018e71696
r-scott25 <rachelbernal2@gmail.com>
2023-03-24 13:18:25 -0500

    initialized reservation and contact info page

---
+++ src/components/ContactInfo.js
@@ -0,0 +1,10 @@
+import ContactInfoStyles from "./ContactInfoStyles.css";
+
+const ContactInfo = () => {
+  return (
+    <>
+    </>
+  );
+}
+
+export default ContactInfo;
---
+++ src/components/Reservations.js
@@ -0,0 +1,10 @@
+import ReservationsStyles from "./ReservationsStyles.css";
+
+const Reservations = () => {
+  return (
+<>
+<Header />
+<Footer />
+</>
+  );
+}
