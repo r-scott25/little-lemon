7fa017478fe222afdccc977a040b8559e7b2ae6b
r-scott25 <rachelbernal2@gmail.com>
2023-04-02 12:57:51 -0500

    css layout for home page hero section

--- src/components/HomepageHero.js
+++ src/components/HomepageHero.js
@@ -6,28 +6,32 @@ import restaurantfood from "../assets/restaurantfood.jpg";
 function HomepageHero() {
   return (
     <section className="heroHome-container">
-
-      <article className="heroIntro">
+      <div>
+        <article className="heroIntro">
           <h1>Little Lemon</h1>
           <h2>Chicago</h2>
           <p>
-            We are a family owned Mediterranean restaurant focused on
+            We are a family owned Mediterranean restaurant focused <br/> on 
             traditional recipes served with a modern twist.
           </p>
+          <div>
+            <Link to="/reservations">
+              <button className="btn" id="smallReserveTableBtn">
+                <a href="/reservations">Reserve a Table</a>
+              </button>
+            </Link>
+          </div>
         </article>
-        <Link to="/reservations">
-          <button className="btn" id="smallReserveTableBtn">
-            <a href="/reservations">Reserve a Table</a>
-          </button>
-        </Link>
-
-      <img
-        className="server-image"
-        src={restaurantfood}
-        alt="restaurant server holding a plate of bread."
-      />
+      </div>
+      <div>
+        <img
+          className="server-image"
+          src={restaurantfood}
+          alt="restaurant server holding a plate of bread."
+        />
+      </div>
     </section>
   );
-};
+}
 
 export default HomepageHero;
