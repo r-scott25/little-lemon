3b6ef4b486641c47679e8b83970831ae4331d990
r-scott25 <rachelbernal2@gmail.com>
2023-04-06 16:57:15 -0500

    fixed nav links/toggle-button

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "./FooterStyles.css";
-import FooterNavigation from "./FooterNavigation.js";
 import logoGreenLemon from "../assets/logoGreenLemon.png";
 import instagram from "../assets/instagram.svg";
 import facebook from "../assets/facebook.svg";
@@ -8,18 +7,53 @@ import twitter from "../assets/twitter.svg";
 
 function Footer() {
   return (
-    <footer>
-      <img className="footer-logo" src={logoGreenLemon} alt="logo" />
-      <FooterNavigation />
-      <section id="contactInfo">
-        <h5 id="contactTitle">Contact</h5>
+    <footer className="footer-container">
+        <img className="footer-logo" src={logoGreenLemon} alt="logo" />
+      <div>
+        <nav className="footer-nav-container">
+          <ul>
+            <li>
+              <a href="/" className="footer-nav-item">
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="/about" className="footer-nav-item">
+                About
+              </a>
+            </li>
+            <li>
+              <a href="/menu" className="footer-nav-item">
+                Menu
+              </a>
+            </li>
+            <li>
+              <a href="/reservations" className="footer-nav-item">
+                Reservations
+              </a>
+            </li>
+            <li>
+              <a href="/order-online" className="footer-nav-item">
+                Order Online
+              </a>
+            </li>
+            <li>
+              <a href="/login" className="footer-nav-item">
+                Login
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <div className="contact-info-container">
+        <h4 id="contactTitle">Contact</h4>
         <p id="lemonAddress">
           123 Lemon St. <br /> Chicago, IL 60654
         </p>
         <p id="lemonPhone">(312) 555-5555</p>
         <p id="lemonEmail">lemon@email.com</p>
-      </section>
-      <section className="socialMedia">
+      </div>
+      <div className="social-media-container">
         <h4 id="let'sConnectTitle">Let's Connect!</h4>
         <ul id="socialMediaList">
           <li id="instagramLink">
@@ -32,9 +66,9 @@ function Footer() {
             <img src={twitter} alt="twitter logo" />
           </li>
         </ul>
-      </section>
+      </div>
     </footer>
   );
-};
+}
 
 export default Footer;
