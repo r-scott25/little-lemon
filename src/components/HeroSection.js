c3103d6db8de9b82df9dde963239f57bafd451f2
r-scott25 <rachelbernal2@gmail.com>
2023-04-11 12:24:37 -0500

    hero section 480 and 600

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -5,9 +5,9 @@ import "./HeroSectionStyles.css";
 function HeroSection(props) {
  return (
   <section className="hero-section-container">
-  <div className="hero-title">
-    <h1>{props.title}</h1>
-    <h2>{props.subtitle}</h2>
+  <div className="hero-title-container">
+    <h1 className="hero-title">{props.title}</h1>
+    <h2 className="hero-subtitle">{props.subtitle}</h2>
   </div>
   </section>
 
